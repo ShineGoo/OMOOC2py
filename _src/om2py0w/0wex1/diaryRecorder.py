3d6992330132c3953f7fa100d77a4e8b10c03974
Lu <janicezeng.12@gmail.com>
2015-10-21 21:42:40 +0800

    update _src/om2py0w/0wex1/README.md, _src/om2py0w/0wex1/diaryRecorder.py, _src/om2py0w/0wex1/myDiary.txt

--- _src/om2py0w/0wex1/diaryRecorder.py
+++ _src/om2py0w/0wex1/diaryRecorder.py
@@ -9,7 +9,7 @@ if diary_existence:
     txt = open("myDiary.txt")
     existingDiaries = txt.read()
     txt.close()
-    print "Hi there, here's your current diary collection:\n%s" % existingDiaries
+    print "Hi there, here's your current diary collection:\n\n%s" % existingDiaries
 else:
     existingDiaries = ""
     print "I cannot find your diary book. No worries tough, ",
@@ -17,13 +17,19 @@ else:
     
 current_date_time = str(datetime.now())
 
-prompt = '>'
-print "Please key in your one line diary for today."
-diary = current_date_time + "\n" + raw_input(prompt) +"\n\n"
+def writeDiary(): 
+    print "Please key in your one line diary for today."
+    diary = current_date_time + "\n" + raw_input('>') +"\n\n"
+    txt2 = open("myDiary.txt", 'a')
+    txt2.write(diary)
+    txt2.close()
+    print "Your new diary has been saved.\nDo you want to write some more?"
+    print "Enter 'y' to record a new line, any other letter to quit."
+    return raw_input('>')
 
-txt2 = open("myDiary.txt", 'a')
-txt2.write(diary)
+write_again = writeDiary()
 
-txt2.close()
-
-
+while write_again == 'y':
+    write_again = writeDiary()
+    
+print "Bye!"
