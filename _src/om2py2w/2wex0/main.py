d1ea9c7e6356a5b4d993861a309a36f6c79a7ff4
Lu <janicezeng.12@gmail.com>
2015-10-28 21:33:52 +0800

    Modify  _src/om2py2w/2wex0/main.py

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # diaryRecorder with GUI by Lu, Oct 26 2015
 # -*- coding: utf-8 -*-
 
@@ -6,10 +7,27 @@ from datetime import datetime
 from Tkinter import *
 import ttk
 
-root = Tk()
 diary_existence = exists("myDiary.txt")
 current_date_time = str(datetime.now())
 
+#construct the main window
+root = Tk()
+#the text widget where existing diaries are shown
+T = Text(root)
+#text above the entry space
+label = ttk.Label(root, text = "\nEnter your one line diary below:")
+#place for user to enter new diary
+entry = ttk.Entry(root, width = 30)
+#in clicking this button the content in entry will be saved
+buttonSubmit = ttk.Button(root, text = "sumbit")
+#the button that closes the window and quit the process
+buttonLeave = ttk.Button(root, text = "leave", command = root.destroy)
+#text that shows after the user clicked the submit button, asking if the user wants to repeat
+label2 = ttk.Label(root, text="")
+buttonContinue = ttk.Button(root, text = "Yes")
+buttonDiscontinue = ttk.Button(root, text = "No", command = root.destroy)
+
+## define a function that reads the content of existing diary and saves it in the returned value
 def readDiary():
     if diary_existence:
         txt = open("myDiary.txt")
@@ -20,7 +38,9 @@ def readDiary():
         existingDiaries = ""
         printOut= '''I cannot find your diary book.\nNo worries tough, I'll create one called 'myDiary.txt' for you.'''
     return printOut
-    
+   
+# this function read the content in the entry space, write it on widget T,
+# and save it to myDiary.txt. It also reveals label2, buttonContinue and buttonDiscontinue 
 def getNewDiary():
     diaryNew = current_date_time + "\n" + entry.get() +"\n\n"
     txt2 = open("myDiary.txt", 'a')
@@ -35,6 +55,7 @@ def getNewDiary():
     buttonContinue.state(['!disabled'])
     buttonDiscontinue.state(['!disabled'])
 
+# defining things that happen after buttonContinue is clicked
 def reEnter():
     entry.delete(0, END)
     buttonSubmit.state(['!disabled'])
@@ -42,33 +63,19 @@ def reEnter():
     buttonDiscontinue.state(['disabled'])
     label2.config(text="")
     
-printOut = readDiary()
 
-root.title("The Diary")
+def foo():
+    root.title("The Diary")
+    T.grid(row=0, column=0, columnspan=2)
+    T.insert(END, readDiary())
+    label.grid(row=1, column=0, columnspan=2)
+    entry.grid(row=2, column=0, columnspan=2)
+    buttonSubmit.grid(row=4, column=0)
+    buttonLeave.grid(row=4, column=1)
+    label2.grid(row=5,column=0, columnspan=2)
+    buttonSubmit.config(command = getNewDiary)
+    buttonContinue.config(command = reEnter)
+    root.mainloop()
 
-T = Text(root)
-T.grid(row=0, column=0, columnspan=2)
-T.insert(END, printOut)
-
-label = ttk.Label(root, text = "\nEnter your one line diary below:", wraplength = 300, background = 'white')
-label.grid(row=1, column=0, columnspan=2)
-
-entry = ttk.Entry(root, width = 30)
-entry.grid(row=2, column=0, columnspan=2)
-
-buttonSubmit = ttk.Button(root, text = "sumbit")
-buttonSubmit.grid(row=4, column=0)
-
-buttonLeave = ttk.Button(root, text = "leave", command = root.destroy)
-buttonLeave.grid(row=4, column=1)
-
-label2 = ttk.Label(root, text="")
-label2.grid(row=4,column=0, columnspan=2)
-
-buttonContinue = ttk.Button(root, text = "Yes")
-buttonDiscontinue = ttk.Button(root, text = "No", command = root.destroy)
-
-buttonSubmit.config(command = getNewDiary)
-buttonContinue.config(command = reEnter)
-
-root.mainloop()
+if __name__ == '__main__':
+    foo()
