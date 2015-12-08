21efb8013f8ab0a8ecd6c6dd6fe281addb588d1b
Lu <janicezeng.12@gmail.com>
2015-12-09 01:01:01 +0800

    edit amazon code

--- 1sTry/amazon/EditorialReviewRetriever.py
+++ 1sTry/amazon/EditorialReviewRetriever.py
@@ -27,6 +27,7 @@ parseER(retrieveER('0230613519'))
     
 #########################################################
 from bs4 import BeautifulSoup
+import requests
 
 def retrieveER_bs(ISBN):
     urlo = 'http://www.amazon.com/gp/product/'
@@ -55,3 +56,13 @@ retrieveER_bs('0230613519')
 #need to refine the scrapper: identify the h3 with class "productDescriptionSource" immediately
 #preceding the div with class "productDescriptionWrapper", in order to distinguish real 
 #editorial reviews from product description and author info. See 0230613519 as an example
+
+##############################################################
+ISBNid = raw_input("input the ISBN, two examples are 0787996491 and 0230613519\n")
+retrieveMethod = raw_input("input 'bot' for useing bottlenose, input 'bs' for using beautifulsoup\n")
+if retrieveMethod == 'bot':
+    parseER(retrieveER(ISBNid))
+elif retrieveMethod == 'bs':
+    retrieveER_bs(ISBNid)
+else:
+    print('Bye')
