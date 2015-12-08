2e199ae21719bae4f73b3c805ab751c82b27120c
Lu <janicezeng.12@gmail.com>
2015-12-09 00:49:56 +0800

    add amazon code

---
+++ 1sTry/amazon/EditorialReviewRetriever.py
@@ -0,0 +1,57 @@
+######################################################
+# use bootlenose
+import bottlenose
+from lxml import etree
+
+AWS_ACCESS_KEY_ID = 'AKIAIWEUU355FB2H7O7Q' #my access key
+AWS_SECRET_ACCESS_KEY = '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' #my secret access key 
+AWS_ASSOCIATE_TAG = 'agathehello-23' #my associate tag
+
+amazon = bottlenose.Amazon(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_ASSOCIATE_TAG)
+
+def retrieveER(ISBN):
+    response = amazon.ItemLookup(ItemId=ISBN, ResponseGroup="EditorialReview", 
+                                 SearchIndex="Books", IdType="ISBN")
+    return response
+    
+def parseER(raw):
+    root = etree.fromstring(raw)   
+    for item in root[1][1:]:
+        print(etree.tostring(item[1]))
+        print('\n')
+    
+    
+parseER(retrieveER('0787996491'))
+parseER(retrieveER('0230613519'))
+# neither search generates desirable result
+    
+#########################################################
+from bs4 import BeautifulSoup
+
+def retrieveER_bs(ISBN):
+    urlo = 'http://www.amazon.com/gp/product/'
+    url = urlo + ISBN + '/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content)
+    f = soup.findAll('div', attrs={'class':'productDescriptionWrapper'})
+    for ele in f:
+        print(ele.text)
+        
+retrieveER_bs('0787996491')
+retrieveER_bs('0230613519')
+
+#issue with beautifulsoup: sometimes not all editorial reviews are showen on the 
+#product page, user has to click a "read all editorial reviews" and then be redirected
+#to another page where the originally hidden editorial reviews are shown. This 'hidden'
+#part appears to be unscrapable for me. See book 0787996491 for an example, and relevant
+#webpage looks like:
+#</div>
+#<div class="seeAll">
+#<a href="http://www.amazon.com/gp/product/product-description/0787996491">See all Editorial Reviews</a>
+#</div> </div>
+#see 0787996491 as an example
+
+
+#need to refine the scrapper: identify the h3 with class "productDescriptionSource" immediately
+#preceding the div with class "productDescriptionWrapper", in order to distinguish real 
+#editorial reviews from product description and author info. See 0230613519 as an example
