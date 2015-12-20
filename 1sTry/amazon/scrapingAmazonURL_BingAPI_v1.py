e4825961d52454a2dbe372a47c6c63efe2fa470f
Lu <janicezeng.12@gmail.com>
2015-12-21 00:12:04 +0800

    edit BING code

--- 1sTry/amazon/scrapingAmazonURL_BingAPI_v1.py
+++ 1sTry/amazon/scrapingAmazonURL_BingAPI_v1.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# based on ver0
+# to use: specify recommender name in line 13; specify the full path of the text file 
+#         containing the Bing Search API Key in line 14; specify the path for your result 
+#          json file in line 15.
 
 import requests
 from requests.auth import HTTPBasicAuth
 import urllib2
 import json
 
-#recommender and search query
-recommeder = 'Steven_Pinker'
-q0 = '"Editorial Reviews" "Steven Pinker" "Books" site:Amazon.com'
+#input area #recommender = 'Steven Pinker' / 'Carol Dweck' / 'Gerd Gigerenzer' / 'James G March'
+recommender = 'James G March'
+API_KEY_path = '/Users/apple/Documents/PythonClass/amazonProj/BingSearchAPIkey.txt'
+result_path = '/Users/apple/Documents/PythonClass/amazonProj/'
+
+#search query
+q0 = '"Editorial Reviews" "'+recommender+'" "Books" site:Amazon.com'
 
 # create search url
 service_root_uri = 'https://api.datamarket.azure.com/Bing/Search/' 
@@ -22,7 +28,7 @@ def formURL(inputskip):
     return urlt
 
 # get json output from a url
-API_KEY = open("/Users/apple/Documents/PythonClass/amazonProj/BingSearchAPIkey.txt").read()
+API_KEY = open(API_KEY_path).read()
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"
 
 def getJson(inputurl):
@@ -30,42 +36,29 @@ def getJson(inputurl):
     auth = HTTPBasicAuth(API_KEY, API_KEY)
     headers = {'User-Agent': user_agent}
     # get response from search url
-    response_data_t = requests.get(inputurl, headers=headers, auth = auth, verify=False)
-    json_result_t = response_data_t.json()
+    try:
+        response_data_t = requests.get(inputurl, headers=headers, auth = auth, verify=False, timeout=10)
+        json_result_t = response_data_t.json()
+    except requests.RequestException as e:
+        json_result_t = None
+        print "There is a connection error. Please check your Internet Connection and run this code later."
     return json_result_t
     
 # get first 50 results and get result count
-skip = 0 #the offset requested for the starting point of results . The default is zero.
+#the offset requested for the starting point of results . The default is zero.
+
+skip = 0 
 url = formURL(skip)
 json_result_1 = getJson(url)
+if json_result_1 is None:
+    quit()
+
 result_count_1 = int(json_result_1['d']['results'][0]['WebTotal'])
-print result_count_1 #6100
+print 'The initial result count is %r' % (result_count_1) #6100
 
 search_result_url_1 = []
 for k in json_result_1['d']['results'][0]['Web']:
     search_result_url_1.append(k['Url'])
-
-####################################
-#search page 2
-page = 2
-skipp = 50*(page-1)
-url = formURL(skipp)
-json_result_p = getJson(url)
-json_result_p['d']['results'][0]['WebTotal'] #6100
-
-page = 20
-skipp = 50*(page-1)
-url = formURL(skipp)
-json_result_p = getJson(url)
-json_result_p['d']['results'][0]['WebTotal'] #6100
-
-page = 120
-skipp = 50*(page-1)
-url = formURL(skipp)
-json_result_p = getJson(url)
-json_result_p['d']['results'][0]['WebTotal'] #'' #hmmm
-json_result_p['d']['results'][0]['WebTotal'] == '' #True
-len(json_result_p['d']['results'][0]['Web']) == 0 #True
 
 ###############################
 #Now I'm ready to scrape all result!
@@ -73,11 +66,14 @@ def getPage(page):
     skipp = 50*(page-1)
     url = formURL(skipp)
     json_result_p = getJson(url)
-    result_count_p = json_result_p['d']['results'][0]['WebTotal']
-    search_result_url_p = []
-    for k in json_result_p['d']['results'][0]['Web']:
-        search_result_url_p.append(k['Url'])
-    datap = {'result_count':result_count_p, 'search_result_url':search_result_url_p}
+    if json_result_p is None:
+        datap = None
+    else:
+        result_count_p = json_result_p['d']['results'][0]['WebTotal']
+        search_result_url_p = []
+        for k in json_result_p['d']['results'][0]['Web']:
+            search_result_url_p.append(k['Url'])
+        datap = {'result_count':result_count_p, 'search_result_url':search_result_url_p}
     return datap
 
 search_result_url = [search_result_url_1]
@@ -85,33 +81,42 @@ result_count = [result_count_1]
 
 for k in range(2, result_count_1/50+2):
     search_result_t = getPage(k)
-    search_count_t = search_result_t['result_count']
-    search_result_url_t = search_result_t['search_result_url']
-    if (search_count_t == '') and (len(search_result_url_t)==0):
-        print "page %r is blank" % (k)
-        break
+    if search_result_t is None:
+        print "There is a connection error. Please check your Internet Connection and run this code later."
+        quit()
     else:
-        search_result_url.append(search_result_url_t)
-        result_count.append(search_count_t)
-        print "page %r obtained" % (k)
+        search_count_t = search_result_t['result_count']
+        search_result_url_t = search_result_t['search_result_url']
+        if (search_count_t == '') and (len(search_result_url_t)==0):
+            print "page %r is blank" % (k)
+            break
+        else:
+            search_result_url.append(search_result_url_t)
+            result_count.append(search_count_t)
+            print "page %r obtained" % (k)
 
 # for Steven Pinker, ... page 21 obtained, page 22 is blank, essentially only 1050 results, not 6100   
 # to see this, print result_count, the 1st to the 20th element of result_count is 6100
 # while the 21st element of result_count is 1000
-len(search_result_url[20]) #49 slightly smaller than 50
+# len(search_result_url[20]) #49 slightly smaller than 50
 
 ################################
 ##save result
 amazonUrlsGeneric = sum(search_result_url, [])
-len(amazonUrlsGeneric) #1099
+#len(amazonUrlsGeneric) #1099
+amazonUrlsGeneric = list(set(amazonUrlsGeneric))
 
 data2 = {'query': q0, 'amazonUrls': amazonUrlsGeneric}
 
-fileName = '/Users/apple/Documents/gitHub/OMOOC2py/1sTry/amazon/' + recommeder+'_BING.json'
+fileName = result_path + recommender.replace(' ', '_')+'_BING.json'
+
 with open(fileName, 'w') as fp:
     json.dump(data2, fp)
     
-#try reloading saved data
+#try reloading saved data and print to screen
 json_data = open(fileName).read()
 datata = json.loads(json_data)
-print(datata)
+for ele in datata['amazonUrls']:
+    print ele
+
+print "%r unique urls obtained for %r" %(len(datata['amazonUrls']), recommender) 
